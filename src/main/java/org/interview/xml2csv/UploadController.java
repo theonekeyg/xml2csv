09d8277089d02e99296623706cac4897ca543cec
theonekeyg <theonekeyg@gmail.com>
2020-11-18 05:04:25 +0300

    format code accorindg to Oracle code conventions: https://www.oracle.com/java/technologies/javase/codeconventions-introduction.html

--- src/main/java/org/interview/xml2csv/UploadController.java
+++ src/main/java/org/interview/xml2csv/UploadController.java
@@ -25,55 +25,58 @@ import org.interview.xml2csv.XMLConverter;
 
 @Controller
 public class UploadController {
-  private final Logger logger = LoggerFactory.getLogger(UploadController.class);
+    private final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
-  @RequestMapping(path="/", method=RequestMethod.GET)
-  public String root() {
-    return "root";
-  }
-
-  @RequestMapping(path="/upload_xml", method=RequestMethod.POST)
-  public ResponseEntity ProcessXML(@RequestParam("file") MultipartFile file) {
-    File xmlfp;
-    Path xmlfp_path;
-    try {
-      // Receive the file
-      xmlfp_path = Files.createTempFile(file.getName(), ".xml");
-      xmlfp = new File(xmlfp_path.toString());
-      file.transferTo(xmlfp);
-    } catch (IOException ex) {
-      logger.error(ex.getMessage());
-      return new ResponseEntity<>(HttpStatus.INSUFFICIENT_STORAGE);
+    @RequestMapping(path="/", method=RequestMethod.GET)
+    public String root() {
+        return "root";
     }
-    XMLConverter converter = new XMLConverter();
-    File csvfp = converter.toCSV(xmlfp, file.getName());
-    if (xmlfp.exists()) xmlfp.delete();
-    return getFileResponse(csvfp);
-  }
 
-  private ResponseEntity<Object> getFileResponse(File fp) {
-    if (fp == null) {
-      logger.info("Bad format received");
-      return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+    @RequestMapping(path="/upload_xml", method=RequestMethod.POST)
+    public ResponseEntity ProcessXML(@RequestParam("file") MultipartFile file) {
+        File xmlfp;
+        Path xmlfpPath;
+        try {
+            /* Receive the file */
+            xmlfpPath = Files.createTempFile(file.getName(), ".xml");
+            xmlfp = new File(xmlfpPath.toString());
+            file.transferTo(xmlfp);
+        } catch (IOException ex) {
+            logger.error(ex.getMessage());
+            return new ResponseEntity<>(HttpStatus.INSUFFICIENT_STORAGE);
+        }
+        XMLConverter converter = new XMLConverter();
+        File csvfp = converter.toCSV(xmlfp, file.getName());
+        if (xmlfp.exists()) xmlfp.delete();
+        return getFileResponse(csvfp);
     }
-    InputStreamResource istream;
-    try {
-      istream = new InputStreamResource(new FileInputStream(fp));
-    } catch (FileNotFoundException ex) {
-      logger.error(ex.getMessage());
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+    private ResponseEntity<Object> getFileResponse(File file_ptr) {
+        if (file_ptr == null) {
+            logger.info("Bad format received");
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+        InputStreamResource istream;
+        try {
+            istream = new InputStreamResource(new FileInputStream(file_ptr));
+        } catch (FileNotFoundException ex) {
+            logger.error(ex.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Content-Disposition",
+                    String.format("attachment; filename=\"%s\"", file_ptr.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        ResponseEntity<Object>
+        response = ResponseEntity.ok().headers(headers)
+                       .contentLength(file_ptr.length())
+                       .contentType(MediaType.parseMediaType("application/txt"))
+                       .body(istream);
+        if (file_ptr.exists()) file_ptr.delete();
+        return response;
     }
-    HttpHeaders headers = new HttpHeaders();
-
-    headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", fp.getName()));
-    headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-    headers.add("Pragma", "no-cache");
-    headers.add("Expires", "0");
-
-    ResponseEntity<Object>
-    response = ResponseEntity.ok().headers(headers).contentLength(fp.length()).contentType(
-        MediaType.parseMediaType("application/txt")).body(istream);
-    if (fp.exists()) fp.delete();
-    return response;
-  }
 }
--- src/main/java/org/interview/xml2csv/XMLConverter.java
+++ src/main/java/org/interview/xml2csv/XMLConverter.java
@@ -19,92 +19,116 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class XMLConverter {
-  private final Logger logger = LoggerFactory.getLogger(XMLConverter.class);
-  private HashSet<String> csv_headers = new HashSet<String>(10);
-  private LinkedList<HashMap<String, String>> csv_values = new LinkedList<HashMap<String, String>>();
-  // javax.xml's parser matches closing node as a Node.TEXT_NODE no matter if it contains
-  // text or not. This workaround is mandatory for skipping these nodes.
-  private final Pattern SKIP_PATTERN = Pattern.compile("\n\\s+");
 
-  private void _parseLoop(Node node, HashMap<String, String> map, String head) {
-    NodeList childs = node.getChildNodes();
-    if (childs.getLength() > 0) {
-      for (int i = 0; i < childs.getLength(); ++i) {
-        String new_head = head.concat(String.format("%s__", node.getNodeName()));
-        _parseLoop(childs.item(i), map, new_head);
-      }
-    } else {
-      Matcher skip_matcher = SKIP_PATTERN.matcher(node.getNodeValue());
-      String node_val = node.getNodeValue();
-      // Decline closing nodes and nodes containing commas
-      if (node.getNodeType() == Node.TEXT_NODE && !skip_matcher.matches() && !node_val.contains(",")) {
-        head = head.substring(0, head.length()-2);
-        if (!csv_headers.contains(head)) {
-          csv_headers.add(head);
+    private final Logger logger = LoggerFactory.getLogger(XMLConverter.class);
+
+    private HashSet<String> csvHeaders = new HashSet<String>(10);
+
+    private LinkedList<HashMap<String, String>> csvValues
+        = new LinkedList<HashMap<String, String>>();
+
+    /*
+     * javax.xml's parser matches closing node as a Node.TEXT_NODE no matter if
+     * it contains text or not. This workaround is mandatory for skipping these
+     * nodes.
+     */
+    private final Pattern SKIP_PATTERN = Pattern.compile("\n\\s+");
+
+    private void parseLoop(Node node, HashMap<String, String> map, String head) {
+        NodeList childs = node.getChildNodes();
+        if (childs.getLength() > 0) {
+            for (int i = 0; i < childs.getLength(); ++i) {
+                String newHead = head.concat(String.format("%s__",
+                                              node.getNodeName()));
+                parseLoop(childs.item(i), map, newHead);
+            }
+        } else {
+            Matcher skipMatcher = SKIP_PATTERN.matcher(node.getNodeValue());
+            String nodeVal = node.getNodeValue();
+            /* Decline closing nodes and nodes containing commas */
+            if (node.getNodeType() == Node.TEXT_NODE && !skipMatcher.matches()) {
+                head = head.substring(0, head.length()-2);
+                if (!csvHeaders.contains(head)) {
+                    csvHeaders.add(head);
+                }
+                map.put(head, nodeVal);
+            }
         }
-        map.put(head, node_val);
-      }
     }
-  }
 
-  private HashMap parseElement(Node node) {
-    HashMap<String, String> map = new HashMap<String, String>();
-    _parseLoop(node, map, "");
-    return map;
-  }
+    private HashMap parseElement(Node node) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        parseLoop(node, map, "");
+        return map;
+    }
 
-  private File dumpToCSV(File csvfp) {
-    try {
-      FileWriter writer = new FileWriter(csvfp);
-      // Write headers
-      for (String header : csv_headers) {
-        writer.write(header + ",");
-      }
-      writer.write("\n");
-      // Write the rest
-      for (HashMap row : csv_values) {
-        for (String header : csv_headers) {
-          String val = "";
-          if (row.containsKey(header)) {
-            val = (String)row.get(header);
-          }
-          writer.write(val + ",");
+    private File dumpToCSV(File csvfp) {
+        try {
+            boolean rowStart = true;
+            FileWriter writer = new FileWriter(csvfp);
+            /* Write headers */
+            for (String header : csvHeaders) {
+                if (rowStart) {
+                    writer.write(header);
+                    rowStart = false;
+                } else {
+                    writer.write("," + header);
+                }
+            }
+            writer.write("\n");
+            rowStart = true;
+            /* Write the rest */
+            for (HashMap row : csvValues) {
+                for (String header : csvHeaders) {
+                    String val = "";
+                    if (row.containsKey(header)) {
+                        val = ((String) row.get(header)).replace(',', (char) 0);
+                    }
+                    if (rowStart) {
+                        writer.write(val);
+                        rowStart = false;
+                    } else {
+                        writer.write("," + val);
+                    }
+                }
+                writer.write("\n");
+                rowStart = true;
+            }
+            writer.flush();
+        } catch (IOException ex) {
+            logger.error(ex.getMessage());
+            return null;
         }
-        writer.write("\n");
-      }
-      writer.flush();
-    } catch (IOException ex) {
-      logger.error(ex.getMessage());
-      return null;
+        return csvfp;
     }
-    return csvfp;
-  }
 
-  public File toCSV(File xmlfp, String base_name) {
-    File csvfp = null;
-    try {
-      csvfp = new File(Files.createTempFile(base_name, ".csv").toString());
-      final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-      final DocumentBuilder builder = factory.newDocumentBuilder();
-      final Document xmldoc = builder.parse(xmlfp);
+    public File toCSV(File xmlfp, String baseName) {
+        File csvfp = null;
+        try {
+            csvfp = new File(Files.createTempFile(baseName, ".csv").toString());
+            final DocumentBuilderFactory factory
+                = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder builder = factory.newDocumentBuilder();
+            final Document xmldoc = builder.parse(xmlfp);
 
-      final NodeList allNodes = xmldoc.getElementsByTagName("*");
-      if (allNodes.getLength() < 3) {
-        csvfp.delete();
-        return null;
-      }
-      Node first_elem = allNodes.item(1);
-      NodeList all_elems = xmldoc.getElementsByTagName(first_elem.getNodeName());
+            final NodeList allNodes = xmldoc.getElementsByTagName("*");
+            if (allNodes.getLength() < 3) {
+                csvfp.delete();
+                return null;
+            }
+            Node firstElem = allNodes.item(1);
+            NodeList allElems = xmldoc.getElementsByTagName(
+                                    firstElem.getNodeName());
 
-      for (int i = 0; i < all_elems.getLength(); ++i) {
-        csv_values.add(parseElement(all_elems.item(i)));
-      }
-    } catch (Exception ex) {
-      logger.error(ex.getMessage());
-      if (csvfp.exists()) csvfp.delete();
-      return null;
+            for (int i = 0; i < allElems.getLength(); ++i) {
+                csvValues.add(parseElement(allElems.item(i)));
+            }
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+            if (csvfp.exists()) csvfp.delete();
+            return null;
+        }
+        return dumpToCSV(csvfp);
     }
-    return dumpToCSV(csvfp);
-  }
 }
 
